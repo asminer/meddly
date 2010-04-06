e44cce711c2b3bbd83184251b7cc61bbdb94184a
Andrew S. Miner <asminer@iastate.edu>
2010-04-06 20:39:48 +0000

    Added example: n-queens solution. Fixed makefile for revision number update :^)
    git-svn-id: svn://svn.code.sf.net/p/meddly/code/trunk@76 14966d26-f10a-4722-9547-d5dcc201c352

---
+++ examples/nqueens.cc
@@ -0,0 +1,278 @@
+
+// $Id$
+
+/*
+    Meddly: Multi-terminal and Edge-valued Decision Diagram LibrarY.
+    Copyright (C) 2009, Iowa State University Research Foundation, Inc.
+
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published 
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+    Builds the set of solutions to the N-queens problem for 
+    user-specified N.
+
+    In other words, finds all possible ways to put N queens onto
+    an NxN chessboard so that no queen is attacking any other queen.
+    Clearly, we cannot have two queens in any given row, therefore
+    every such solution must have exactly one queen in each row.
+    We use an MDD with N variables, with variable x_i indicating
+    the column position (from 1 to N) for the queen in row i.
+*/
+
+#include <cstdio>
+
+#include "../include/meddly.h"
+
+int N;
+int* scratch;
+compute_manager* CM;
+
+void printmem(long m)
+{
+  if (m<1024) {
+    printf("%d bytes", m);
+    return;
+  }
+  double approx = m;
+  approx /= 1024;
+  if (approx < 1024) {
+    printf("%3.2lf Kbytes", approx);
+    return;
+  }
+  approx /= 1024;
+  if (approx < 1024) {
+    printf("%3.2lf Mbytes", approx);
+    return;
+  }
+  approx /= 1024;
+  if (approx < 1024) {
+    printf("%3.2lf Gbytes", approx);
+    return;
+  }
+  approx /= 1024;
+  printf("%3.2lf Tbytes", approx);
+}
+
+
+#define LINEAR_INTERSECTIONS
+#ifdef  LINEAR_INTERSECTIONS
+
+void intersect(dd_edge** A, int L)
+{
+  if (0==A[0]) {
+    // find first non-zero and swap
+    for (int i=1; i<L; i++) {
+      if (A[i]) {
+        A[0] = A[i];
+        A[i] = 0;
+        break;
+      }
+    }
+    if (0==A[0]) return;
+  }
+  fprintf(stderr, "\t");
+  for (int i=1; i<L; i++) {
+    fprintf(stderr, "%d ", L-i);
+    if (A[i]) {
+      assert(
+        compute_manager::SUCCESS ==
+        CM->apply(compute_manager::MULTIPLY, *A[0], *A[i], *A[0])
+      );
+      delete A[i];
+      A[i] = 0;
+    }
+  }
+  fprintf(stderr, "\n");
+}
+
+#else 
+
+void intersect(dd_edge** A, int L)
+{
+  while (L>1) {
+    fprintf(stderr, "\t%2d terms to combine ", L);
+    // combine adjacent pairs
+    for (int i=0; i<L; i+=2) {
+      if (A[i] && A[i+1]) {
+        assert(
+          compute_manager::SUCCESS ==
+          CM->apply(compute_manager::MULTIPLY, *A[i], *A[i+1], *A[i])
+        );
+        delete A[i+1];
+        A[i+1] = 0;
+        fprintf(stderr, ".");
+      }
+    } // for i
+    fprintf(stderr, "\n");
+    // compact
+    int p = 0;
+    for (int i=0; i<L; i++) {
+      if (A[i]) {
+        if (i>p) {
+          A[p] = A[i];
+          A[i] = 0;
+        }
+        p++;
+      }
+    } // for i
+    L = p;
+  } // while
+}
+
+#endif
+
+
+bool createQueenNodes(forest* f, int q, dd_edge &col, dd_edge &cp, dd_edge &cm)
+{
+  assert(q>0);
+  assert(q<=N);
+  bool error = f->createEdgeForVar(q, false, col);
+  if (error) return error;
+  for (int i=0; i<N; i++) {
+    scratch[i] = i+q;
+  }
+  error = f->createEdgeForVar(q, false, scratch, cp);
+  if (error) return error;
+  for (int i=0; i<N; i++) {
+    scratch[i] = i-q;
+  }
+  error = f->createEdgeForVar(q, false, scratch, cm);
+  return error;
+}
+
+int main()
+{
+  CM = MEDDLY_getComputeManager();
+  assert(CM);
+  printf("Using %s\n", MEDDLY_getLibraryInfo(0));
+  printf("N-Queens solutions.  Enter the value for N:\n");
+  scanf("%d", &N);
+  if (N<1) return 0;
+  scratch = new int[N+1];
+  
+  printf("Initializing domain and forest\n");
+
+  for (int i=0; i<N; i++) {
+    scratch[i] = N;
+  }
+  domain* d = MEDDLY_createDomain();
+  assert(d);
+  assert(domain::SUCCESS == d->createVariablesBottomUp(scratch, N));
+  forest* f = d->createForest(false, forest::INTEGER, forest::MULTI_TERMINAL);
+  assert(f);
+
+  // Set up MDD options
+  assert(forest::SUCCESS == f->setReductionRule(forest::FULLY_REDUCED));
+  assert(forest::SUCCESS == f->setNodeStorage(forest::FULL_OR_SPARSE_STORAGE));
+  assert(forest::SUCCESS == f->setNodeDeletion(forest::PESSIMISTIC_DELETION));
+
+  printf("Building nodes for queen column and diagonals\n");
+
+  dd_edge** col = new dd_edge*[N];
+  dd_edge** dgp = new dd_edge*[N];
+  dd_edge** dgm = new dd_edge*[N];
+  dd_edge** constr = new dd_edge*[N+1];
+
+  for (int i=0; i<N; i++) {
+    col[i] = new dd_edge(f);
+    dgp[i] = new dd_edge(f);
+    dgm[i] = new dd_edge(f);
+    assert(false == createQueenNodes(f, i+1, *col[i], *dgp[i], *dgm[i]));
+    constr[i] = new dd_edge(f);
+    assert(forest::SUCCESS == f->createEdge(int(1), *constr[i]));
+  }
+  constr[N] = 0;
+
+  for (int i=0; i<N-1; i++) {
+    printf("Building queen %2d constraints\n", i+1);
+    for (int j=N-1; j>i; j--) {
+      dd_edge uniq_col(f);
+      assert(
+        compute_manager::SUCCESS ==
+        CM->apply(compute_manager::NOT_EQUAL, *col[i], *col[j], uniq_col)
+      );
+      dd_edge uniq_dgp(f);
+      assert(
+        compute_manager::SUCCESS ==
+        CM->apply(compute_manager::NOT_EQUAL, *dgp[i], *dgp[j], uniq_dgp)
+      );
+      dd_edge uniq_dgm(f);
+      assert(
+        compute_manager::SUCCESS ==
+        CM->apply(compute_manager::NOT_EQUAL, *dgm[i], *dgm[j], uniq_dgm)
+      );
+      // build overall "not attacking each other" set...
+      assert(
+        compute_manager::SUCCESS ==
+        CM->apply(compute_manager::MULTIPLY, uniq_col, uniq_dgp, uniq_col)
+      );
+      assert(
+        compute_manager::SUCCESS ==
+        CM->apply(compute_manager::MULTIPLY, uniq_col, uniq_dgm, uniq_col)
+      );
+      int k = uniq_col.getLevel()-1;
+      assert(k>=0);
+      assert(k<N);
+      assert(
+        compute_manager::SUCCESS ==
+        CM->apply(compute_manager::MULTIPLY, *constr[k], uniq_col, *constr[k])
+      );
+    } // for j
+  } // for i
+  printf("Building solutions\n");
+  intersect(constr, N);
+  assert(constr[0]);
+  dd_edge solutions = *constr[0];
+
+  printf("Cleanup\n");
+  for (int i=0; i<N; i++) {
+    delete col[i];
+    delete dgp[i];
+    delete dgm[i];
+    delete constr[i];
+  }
+  delete[] col;
+  delete[] dgp;
+  delete[] dgm;
+  delete[] constr;
+
+  printf("Done.\n\n");
+
+  printf("Set of solutions requires %d nodes\n", solutions.getNodeCount());
+  printf("Forest stats:\n");
+  printf("\t%d current nodes\n", f->getCurrentNumNodes());
+  printf("\t%d peak nodes\n", f->getPeakNumNodes());
+  printf("\t");
+  printmem(f->getCurrentMemoryUsed());
+  printf(" current memory\n\t");
+  printmem(f->getPeakMemoryUsed());
+  printf(" peak memory\n");
+
+  double c = solutions.getCardinality();
+  printf("\nThere are %lg solutions to the %d-queens problem\n\n", c, N);
+
+  // show one of the solutions
+  dd_edge::const_iterator first = solutions.begin();
+  dd_edge::const_iterator endIt = solutions.end();
+  if (first != endIt) {
+    const int* minterm = first.getAssignments();
+    printf("One solution:\n");
+    for (int i=1; i<=N; i++) {
+      printf("\tQueen for row %2d in column %2d\n", i, minterm[i]+1);
+    }
+  }
+
+  return 0;
+}
