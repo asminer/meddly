c3fec1961fc63738f17df8b778bd54f874ed3e59
Andrew S. Miner <asminer@iastate.edu>
2017-07-03 21:16:24 +0000

    Added benchmark test files to tagged versions
    git-svn-id: svn://svn.code.sf.net/p/meddly/code/tags/v0.03@818 14966d26-f10a-4722-9547-d5dcc201c352

---
+++ examples/kanban.cc
@@ -0,0 +1,114 @@
+
+// $Id: kanban.cc 233 2011-07-05 14:20:36Z asminer $
+
+/*
+    Meddly: Multi-terminal and Edge-valued Decision Diagram LibrarY.
+    Copyright (C) 2011, Iowa State University Research Foundation, Inc.
+
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published 
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "meddly.h"
+#include "simple_model.h"
+
+const char* kanban[] = {
+  "X-+..............",  // Tin1
+  "X.-+.............",  // Tr1
+  "X.+-.............",  // Tb1
+  "X.-.+............",  // Tg1
+  "X.....-+.........",  // Tr2
+  "X.....+-.........",  // Tb2
+  "X.....-.+........",  // Tg2
+  "X+..--+..-+......",  // Ts1_23
+  "X.........-+.....",  // Tr3
+  "X.........+-.....",  // Tb3
+  "X.........-.+....",  // Tg3
+  "X....+..-+..--+..",  // Ts23_4
+  "X.............-+.",  // Tr4
+  "X.............+-.",  // Tb4
+  "X............+..-",  // Tout4
+  "X.............-.+"   // Tg4
+};
+
+int usage(const char* name)
+{
+  printf("\nUsage: %s nnnn (-bfs) (-dfs)\n\n", name);
+  printf("\tnnnn: number of parts\n");
+  printf("\t-bfs: use traditional iterations\n");
+  printf("\t-dfs: use saturation\n\n");
+  return 1;
+}
+
+int main(int argc, const char** argv)
+{
+  int N = -1;
+  bool useSaturation = true;
+
+  for (int i=1; i<argc; i++) {
+    if (strcmp("-bfs", argv[i])==0) {
+      useSaturation = false;
+      continue;
+    }
+    if (strcmp("-dfs", argv[i])==0) {
+      useSaturation = true;
+      continue;
+    }
+    N = atoi(argv[i]);
+  }
+
+  if (N<0) return usage(argv[0]);
+
+  // Initialize domain
+  int* sizes = new int[16];
+  for (int i=15; i>=0; i--) sizes[i] = N+1;
+  domain* d = MEDDLY_createDomain();
+  d->createVariablesBottomUp(sizes, 16);
+
+  // Build initial state
+  int* initial = new int[17];
+  for (int i=16; i; i--) initial[i] = 0;
+  initial[1] = initial[5] = initial[9] = initial[13] = N;
+  forest* mdd = d->createForest(0, forest::BOOLEAN, forest::MULTI_TERMINAL);
+  dd_edge init_state(mdd);
+  mdd->createEdge(&initial, 1, init_state);
+
+  // Build next-state function
+  forest* mxd = d->createForest(1, forest::BOOLEAN, forest::MULTI_TERMINAL);
+  dd_edge nsf(mxd);
+  buildNextStateFunction(kanban, 16, mxd, nsf, 4);
+
+  printf("Building reachable states\n");
+  fflush(stdout);
+
+  dd_edge reachable(mdd);
+  compute_manager* CM = MEDDLY_getComputeManager();
+  if (useSaturation)
+    CM->apply(compute_manager::REACHABLE_STATES_DFS, init_state, nsf, reachable);
+  else
+    CM->apply(compute_manager::REACHABLE_STATES_BFS, init_state, nsf, reachable);
+
+  printf("Done\n");
+  double c;
+  CM->apply(compute_manager::CARDINALITY, reachable, c);
+  printf("Approx. %g reachable states\n", c);
+  
+  // cleanup
+  delete d;
+  return 0;
+}
+
---
+++ examples/simple_model.cc
@@ -0,0 +1,169 @@
+
+// $Id: simple_model.cc 233 2011-07-05 14:20:36Z asminer $
+
+/*
+    Meddly: Multi-terminal and Edge-valued Decision Diagram LibrarY.
+    Copyright (C) 2011, Iowa State University Research Foundation, Inc.
+
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published 
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "meddly.h"
+#include "meddly_expert.h"
+#include "simple_model.h"
+
+#define VERBOSE
+
+inline int MAX(int a, int b) {
+  return (a>b) ? a : b;
+}
+
+void buildNextStateFunction(const char* const* events, int nEvents,
+  forest* mxd, dd_edge &nsf, int verb)
+{
+  if (verb) fprintf(stderr, "Building next-state function\n");
+
+  compute_manager* CM = MEDDLY_getComputeManager();
+
+  // set up auxiliary mtmxd forest and edges
+  domain* d = mxd->useDomain();
+  forest* mtmxd = d->createForest(
+    true, forest::INTEGER, forest::MULTI_TERMINAL
+  );
+  int nVars = d->getNumVariables();
+  int maxBound = d->getVariableBound(1, false);
+  for (int i=2; i<=nVars; i++) {
+    maxBound = MAX(maxBound, d->getVariableBound(i, false));
+  }
+  maxBound++;
+  int* temp = new int[maxBound];
+  int* minterm = new int[nVars+1];
+  int* mtprime = new int[nVars+1];
+  dd_edge** varP  = new dd_edge*[nVars+1];
+  varP[0] = 0;
+  dd_edge** inc   = new dd_edge*[nVars+1];
+  inc[0] = 0;
+  dd_edge** dec   = new dd_edge*[nVars+1];
+  dec[0] = 0;
+
+  //  Create edge for each variable xi'
+  for (int i=1; i<=nVars; i++) {
+    varP[i] = new dd_edge(mtmxd);
+    mtmxd->createEdgeForVar(i, true, varP[i][0]);
+  }
+
+  // Create edge for each function xi+1
+  for (int i=0; i<maxBound; i++) temp[i] = i+1;
+  for (int i=1; i<=nVars; i++) {
+    inc[i] = new dd_edge(mtmxd);
+    mtmxd->createEdgeForVar(i, false, temp, inc[i][0]);
+  }
+
+  // Create edge for each function xi-1
+  for (int i=0; i<maxBound; i++) temp[i] = i-1;
+  for (int i=1; i<=nVars; i++) {
+    dec[i] = new dd_edge(mtmxd);
+    mtmxd->createEdgeForVar(i, false, temp, dec[i][0]);
+  }
+
+  mxd->createEdge(false, nsf);
+
+  for (int e=0; e<nEvents; e++) {
+    const char* ev = events[e];
+    if (verb>1) fprintf(stderr, "Event %5d", e);
+
+    dd_edge nsf_ev(mxd);
+    dd_edge term(mxd);
+
+    //
+    // build mask for this event
+    //
+    for (int i=1; i<=nVars; i++) {
+      if ('.' == ev[i]) {
+        minterm[i] = -2;
+        mtprime[i] = -2;
+      } else {
+        minterm[i] = -1;
+        mtprime[i] = -1;
+      }
+    }
+    mxd->createEdge(&minterm, &mtprime, 1, nsf_ev);
+#ifdef DEBUG_EVENTS
+    printf("Initial nsf for event %d\n", e);
+    nsf_ev.show(stdout, 2);
+#endif
+    if (verb>2) fprintf(stderr, " : ");
+    
+    //
+    // 'and' with the "do care" levels
+    //
+    for (int i=1; i<=nVars; i++) {
+      dd_edge docare(mtmxd);
+
+      if ('.' == ev[i]) {
+        if (verb>3) fputc('.', stderr);
+        continue;
+      } else {
+        if (verb>2) fputc(ev[i], stderr);
+      }
+      switch (ev[i]) {
+        case '+':   CM->apply(compute_manager::EQUAL, varP[i][0], inc[i][0], docare);
+                    break;
+
+        case '-':   CM->apply(compute_manager::EQUAL, varP[i][0], dec[i][0], docare);
+                    break;
+
+        default:    throw 1;
+      } // switch
+      CM->apply(compute_manager::COPY, docare, term);
+#ifdef DEBUG_EVENTS
+      printf("Term for event %d, level %d\n", e, i);
+      term.show(stdout, 2);
+#endif
+      nsf_ev *= term;
+    } // for i
+
+    //
+    //  union with overall
+    //
+#ifdef DEBUG_EVENTS
+    printf("Complete nsf for event %d:\n", e);
+    nsf_ev.show(stdout, 2);
+#endif
+    if (verb>2) fputc(' ', stderr);
+    nsf += nsf_ev;
+
+    if (verb>1) fputc('\n', stderr);
+  } // for e
+
+  // cleanup
+  delete[] mtprime;
+  delete[] minterm;
+  delete[] temp;
+  for (int i=1; i<=nVars; i++) {
+    delete varP[i];
+    delete inc[i];
+    delete dec[i];
+  }
+  delete[] varP;
+  delete[] inc;
+  delete[] dec;
+  delete mtmxd;
+
+#ifdef DEBUG_EVENTS
+  printf("Complete NSF:\n");
+  nsf.show(stdout, 2);
+#endif
+}
+
