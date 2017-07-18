8614de6155516a79285300fd84df506cc3a81a9f
Andrew Miner <asminer@iastate.edu>
2017-07-18 14:35:35 -0500

    Simplified libinfo

--- examples/libinfo.cc
+++ examples/libinfo.cc
@@ -37,7 +37,7 @@ int main(int argc, const char** argv)
       long N = atol(argv[i]);
       const char* info = getLibraryInfo(N);
       if (info) {
-        cout << getLibraryInfo(N) << "\n";
+        cout << info << "\n";
       } else {
         cout << "(null string)\n";
       }
