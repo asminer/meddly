9c6fe3e24956841d84ea5ded9f09bb31747c729b
Junaid Babar <junaid@iastate.edu>
2017-11-01 20:43:34 -0500

    Added specialized XDD version for MXD union.

--- src/operations/union.cc
+++ src/operations/union.cc
@@ -109,6 +109,11 @@ MEDDLY::union_mxd::union_mxd(const binary_opname* opcode,
   : generic_binary_mxd(opcode, arg1, arg2, res)
 {
   operationCommutes();
+#if 0
+#ifdef USE_XDDS
+  virtual MEDDLY::node_handle compute(node_handle a, node_handle b);
+#endif
+#endif
 }
 
 bool MEDDLY::union_mxd::checkTerminals(node_handle a, node_handle b, node_handle& c)
@@ -147,6 +152,135 @@ bool MEDDLY::union_mxd::checkTerminals(node_handle a, node_handle b, node_handle
   }
   return false;
 }
+
+#if 0
+#ifdef USE_XDDS
+MEDDLY::node_handle 
+MEDDLY::union_mxd::compute(node_handle a, node_handle b) 
+{
+  //  Compute for the unprimed levels.
+  //
+  node_handle result = 0;
+  if (checkTerminals(a, b, result))
+    return result;
+
+  compute_table::search_key* Key = findResult(a, b, result);
+  if (0==Key) return result;
+
+  // Get level information
+  const int aLevel = arg1F->getNodeLevel(a);
+  const int bLevel = arg2F->getNodeLevel(b);
+  int resultLevel = ABS(topLevel(aLevel, bLevel));
+  const int dwnLevel = resF->downLevel(resultLevel);
+
+  // Initialize readers
+  unpacked_node *A = (aLevel < resultLevel) 
+    ? unpacked_node::newRedundant(arg1F, resultLevel, a, false)
+    : unpacked_node::newFromNode(arg1F, a, false)
+    ;
+  const node_handle A_ext_d = A->isExtensible()? A->ext_d(): 0;
+  int last_nz = A->getNNZs()-1;
+  for ( ; last_nz >= 0 && A->d(last_nz) == 0; last_nz--);
+  const int A_last_index = last_nz >= 0? A->i(last_nz): -1;
+
+  unpacked_node *B = (bLevel < resultLevel)
+    ? unpacked_node::newRedundant(arg2F, resultLevel, b, false)
+    : unpacked_node::newFromNode(arg2F, b, false)
+    ;
+  const node_handle B_ext_d = B->isExtensible()? B->ext_d(): 0;
+  last_nz = B->getNNZs()-1;
+  for ( ; last_nz >= 0 && B->d(last_nz) == 0; last_nz--);
+  const int B_last_index = last_nz >= 0? B->i(last_nz): -1;
+
+  const int max_a_b_last_index = MAX(A_last_index, B_last_index);
+
+  int resultSize = A->getNNZs() + B->getNNZs() + 1 + 1;
+  unpacked_node* C = unpacked_node::newSparse(resF, resultLevel, resultSize);
+
+  int nnz = 0;
+  int A_curr_index = 0;
+  int B_curr_index = 0;
+  for ( ; A_curr_index < A->getNNZs() && B_curr_index < B->getNNZs(); ) {
+    // get a_i, a_d, b_i, b_d
+    int a_i, a_d, b_i, b_d;
+    int a_i = A->i(A_curr_index);
+    int b_i = B->i(B_curr_index);
+    if (a_i <= b_i) {
+      a_d = A->d(A_curr_index);
+      A_curr_index++;
+    } else {
+      a_d = 0;
+    }
+    if (a_i >= b_i) {
+      b_d = B->d(B_curr_index);
+      B_curr_index++;
+    } else {
+      b_d = 0;
+    }
+
+    MEDDLY_DCASSERT(a_d != 0 || b_d != 0);
+
+    // compute union(a_d, b_d)
+    int index = (a_d? a_i: b_i);
+    node_handle down = compute_r(index, dwnLevel, a_d, b_d);
+
+    // if union is non-zero, add it to the new node
+    if (down) {
+      C->i_ref(nnz) = index;
+      C->d_ref(nnz) = down;
+      nnz++;
+    }
+  }
+  for ( ; A_curr_index < A->getNNZs(); A_curr_index++) {
+    // do union(a_i, b_ext_i)
+    int index = A->i(A_curr_index);
+    node_handle down = compute_r(index, dwnLevel, A->d(A_curr_index), B_ext_d);
+    if (down) {
+      C->i_ref(nnz) = index;
+      C->d_ref(nnz) = down;
+      nnz++;
+    }
+  }
+  for ( ; B_curr_index < B->getNNZs(); B_curr_index++) {
+    // do union(a_ext_i, b_i)
+    int index = B->i(B_curr_index);
+    node_handle down = compute_r(index, dwnLevel, A_ext_d, B->d(B_curr_index));
+    if (down) {
+      C->i_ref(nnz) = index;
+      C->d_ref(nnz) = down;
+      nnz++;
+    }
+  }
+  if (A->isExtensible() || B->isExtensible()) {
+    int index = max_a_b_last_index+1;
+    int down = compute_r(index, dwnLevel, A_ext_d, B_ext_d)
+    if (down) {
+      C->i_ref(nnz) = index;
+      C->d_ref(nnz) = down;
+      C->markAsExtensible();
+      nnz++;
+    } else {
+      C->markAsNotExtensible();
+    }
+  }
+  C->shrinkSparse(nnz);
+
+  // cleanup
+  unpacked_node::recycle(B);
+  unpacked_node::recycle(A);
+
+  // reduce and save result
+  result = resF->createReducedNode(-1, C);
+  saveResult(Key, a, b, result);
+
+#ifdef TRACE_ALL_OPS
+  printf("computed %s(%d, %d) = %d\n", getName(), a, b, result);
+#endif
+
+  return result;
+}
+#endif
+#endif
 
 // ******************************************************************
 // *                                                                *
