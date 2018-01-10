e1cc7a8688e7e0596f9cca6c535573c4899482a6
sbiswal92 <sbiswal@iastate.edu>
2018-01-10 13:19:55 -0600

    Changes to convert implicit relation into transition relations

--- src/meddly_expert.h
+++ src/meddly_expert.h
@@ -3285,10 +3285,6 @@ class MEDDLY::satimpl_opname:public specialized_opname {
          Set the tokenUpdate array at location i to val
          */
         void setTokenUpdateAtIndex(long i,long val);
-        /**
-         Get arc count
-         */
-        long getArcCounts();
         
         // the following must be provided in derived classes.
         
@@ -3453,6 +3449,11 @@ class MEDDLY::satimpl_opname:public specialized_opname {
       public:
         
         /*
+         Get total number of events upto given level
+         */
+        long getTotalEvent(int level);
+        
+        /*
          Resizes the Event List
          */
         void resizeEventArray(int level);
@@ -3472,10 +3473,20 @@ class MEDDLY::satimpl_opname:public specialized_opname {
           Prints the implicit relation
          */
         void show();
+        
         /*
-         Gets the arc counts from all implicit nodes
+         Build mxd forest
          */
-        long getAllArcCounts(int level);
+        dd_edge buildMxdForest();
+        
+        /*
+         Build each event_mxd
+         */
+        dd_edge buildEventMxd(rel_node_handle event_top, forest *mxd, forest *event_mxd);
+    
+        
+      private:
+        dd_edge mxd;
         
       };  // class implicit_relation
 
