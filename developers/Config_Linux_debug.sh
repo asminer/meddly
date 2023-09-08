40a67dbddf480e6f94242dc701b6234bbffc87b4
Andrew Miner <asminer@iastate.edu>
2023-09-07 21:25:26 -0400

    Relaxed configs until we get warnings fixed

--- developers/Config_Linux_debug.sh
+++ developers/Config_Linux_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Configuring with development tests, debug info
-./configure CXXFLAGS="-Wsign-conversion -Wshadow -ggdb -DDEVELOPMENT_CODE"
+./configure CXXFLAGS="-ggdb -DDEVELOPMENT_CODE"
--- developers/Config_Mac_debug.sh
+++ developers/Config_Mac_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Configuring with development tests, debug info
-./configure CXXFLAGS="-Wsign-conversion -Wshadow -ggdb -DDEVELOPMENT_CODE" LDFLAGS=-L/opt/local/lib CPPFLAGS=-I/opt/local/include
+./configure CXXFLAGS="-ggdb -DDEVELOPMENT_CODE" LDFLAGS=-L/opt/local/lib CPPFLAGS=-I/opt/local/include
