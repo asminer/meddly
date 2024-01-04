152f5db4515f73bedf730ba44c99ec0dff9d67c2
Andrew Miner <asminer@iastate.edu>
2024-01-04 16:05:52 -0600

    Better homebrew vs macports support in Config scripts :)

--- developers/Config_Mac_optimized.sh
+++ developers/Config_Mac_optimized.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
 
-echo Configuring with development tests off, compiler optimizations on
-./configure CXXFLAGS="-O3" LDFLAGS=-L/opt/local/lib CPPFLAGS=-I/opt/local/include
+# Look for gmp
+# Args: directories to search
+#   will echo the first matching directory.
+#   If none, then error out.
+#
+find_gmp_path()
+{
+    for args; do
+        echo "Looking for gmp in $args" > /dev/stderr
+        if [ ! -d $args ]; then
+            continue
+        fi
+        echo "    directory exists" > /dev/stderr
+        if [ ! -d $args/include ]; then
+            continue
+        fi
+        echo "    include/ exists" > /dev/stderr
+        if [ ! -d $args/lib ]; then
+            continue
+        fi
+        echo "    lib/ exists" > /dev/stderr
+        if [ ! -f $args/include/gmp.h ]; then
+            continue
+        fi
+        echo "    include/gmp.h exists" > /dev/stderr
+        if [ ! -f $args/lib/libgmp.a ]; then
+            continue
+        fi
+        echo "    lib/libgmp.a exists" > /dev/stderr
+        echo $args
+        return 0
+    done
+}
+
+GMP=`find_gmp_path /opt/local /opt/homebrew`
+if [ ! "$GMP" ]; then
+    echo
+    echo "Could not find gmp"
+    echo
+    exit 1
+fi
+
+./configure CXXFLAGS="-O3" LDFLAGS=-L/$GMP/lib CPPFLAGS=-I/$GMP/include
+
+echo
+echo Configured using $GMP for gmp, with development tests off,
+echo compiler optimizations on
+echo
+
