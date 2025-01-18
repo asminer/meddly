81334953194be0c3bd640a949a67c080e9be1f62
Andrew Miner <asminer@iastate.edu>
2025-01-17 23:33:39 -0600

    Draft of push-down edge value operations

--- src/operations/arith.cc
+++ src/operations/arith.cc
@@ -40,8 +40,8 @@ namespace MEDDLY {
 };
 
 
-// ******************************************************************
-// ******************************************************************
+//   **************************************************************
+//  ****************************************************************
 // ******************************************************************
 // ***                                                            ***
 // ***                                                            ***
@@ -49,16 +49,16 @@ namespace MEDDLY {
 // ***                                                            ***
 // ***                                                            ***
 // ******************************************************************
-// ******************************************************************
-// ******************************************************************
+//  ****************************************************************
+//   **************************************************************
 
-// ******************************************************************
+//  ****************************************************************
 // *                                                                *
 // *                                                                *
 // *                       arith_compat class                       *
 // *                                                                *
 // *                                                                *
-// ******************************************************************
+//  ****************************************************************
 
 /*
     Template class for "compatible" edge-valued,
@@ -511,13 +511,13 @@ void MEDDLY::arith_compat<EOP, ATYPE>::_compute(int L, unsigned in,
     }
 }
 
-// ******************************************************************
+//  ****************************************************************
 // *                                                                *
 // *                                                                *
 // *                       arith_factor class                       *
 // *                                                                *
 // *                                                                *
-// ******************************************************************
+//  ****************************************************************
 
 /*
     Template class for "factorable" edge-valued,
@@ -823,7 +823,7 @@ void MEDDLY::arith_factor<EOP, ATYPE>::_compute(int L, unsigned in,
     }
     key[keyind++].setN(A);
     key[keyind++].set(bv);
-    key[keyind]  .set(B);
+    key[keyind]  .setN(B);
 
     if (ct->findCT(key, res)) {
         //
@@ -978,13 +978,13 @@ void MEDDLY::arith_factor<EOP, ATYPE>::_compute(int L, unsigned in,
     EOP::accumulateOp(cv, fac);
 }
 
-// ******************************************************************
+//  ****************************************************************
 // *                                                                *
 // *                                                                *
 // *                       arith_pushdn class                       *
 // *                                                                *
 // *                                                                *
-// ******************************************************************
+//  ****************************************************************
 
 /*
     Template class for edge-valued, element-wise arithmetic operations
@@ -1039,10 +1039,397 @@ void MEDDLY::arith_factor<EOP, ATYPE>::_compute(int L, unsigned in,
                           edge_value &a, node_handle &b);
 
  */
+namespace MEDDLY {
+    template <class EOP, class ATYPE>
+    class arith_pushdn : public binary_operation {
+        public:
+            arith_pushdn(forest* arg1, forest* arg2, forest* res);
+            virtual ~arith_pushdn();
 
+            virtual void compute(int L, unsigned in,
+                    const edge_value &av, node_handle ap,
+                    const edge_value &bv, node_handle bp,
+                    edge_value &cv, node_handle &cp);
+
+        protected:
+            // Avoid overhead of a virtual call during recursion
+            void _compute(int L, unsigned in,
+                    edge_value av, node_handle ap,
+                    edge_value bv, node_handle bp,
+                    edge_value &cv, node_handle &cp);
+
+        private:
+
+            inline int topLevelOf(int L, int alevel, int blevel) const
+            {
+                if (forced_by_levels) return L;
+                if (resF->isForRelations()) {
+                    return MXD_levels::topLevel(alevel, blevel);
+                } else {
+                    return MDD_levels::topLevel(alevel, blevel);
+                }
+            }
+
+        private:
+            ct_entry_type* ct;
+            unary_operation* copy_arg1res;
+            unary_operation* copy_arg2res;
+#ifdef TRACE
+            ostream_output out;
+            unsigned top_count;
+#endif
+            bool forced_by_levels;
+    };
+};
 
 // ******************************************************************
-// ******************************************************************
+
+template <class EOP, class ATYPE>
+MEDDLY::arith_pushdn<EOP, ATYPE>::arith_pushdn(forest* arg1, forest* arg2,
+        forest* res) : binary_operation(arg1, arg2, res)
+#ifdef TRACE
+      , out(std::cout), top_count(0)
+#endif
+{
+    checkDomains(__FILE__, __LINE__);
+    if (res->isForRelations()) {
+        checkAllRelations(__FILE__, __LINE__, RELATION);
+    } else {
+        checkAllRelations(__FILE__, __LINE__, SET);
+    }
+    checkAllLabelings(__FILE__, __LINE__, edge_labeling::MULTI_TERMINAL);
+
+    if (arg1->getRangeType() != arg2->getRangeType()) {
+        throw error(error::TYPE_MISMATCH, __FILE__, __LINE__);
+    }
+
+    //
+    // Quick copying, even across forests, for terminal cases :)
+    //
+    copy_arg1res = COPY(arg1, res);
+    copy_arg2res = COPY(arg2, res);
+
+    //
+    // Do we need to recurse by levels and store level info in the CT?
+    // YES, if either forest is identity-reduced
+    //      (even if the other is quasi-reduced, because we can
+    //       still jump to terminal 0)
+    //
+    forced_by_levels = arg1->isIdentityReduced() || arg2->isIdentityReduced();
+
+    // Build compute table key and result types.
+    // If we recurse by levels, then we need the level as part of the key.
+    ct = new ct_entry_type(ATYPE::name());
+    if (forced_by_levels) {
+        ct->appendFixed('I');
+    }
+    // Left operand edge
+    ct->appendFixed(EOP::edgeValueTypeLetter());
+    ct->appendFixed(arg1);
+    // Right operand edge
+    ct->appendFixed(EOP::edgeValueTypeLetter());
+    ct->appendFixed(arg2);
+
+    ct->setResult(EOP::edgeValueTypeLetter(), res);
+    ct->doneBuilding();
+}
+
+template <class EOP, class ATYPE>
+MEDDLY::arith_pushdn<EOP, ATYPE>::~arith_pushdn()
+{
+    ct->markForDestroy();
+}
+
+template <class EOP, class ATYPE>
+void MEDDLY::arith_pushdn<EOP, ATYPE>::compute(int L, unsigned in,
+        const edge_value &av, node_handle ap,
+        const edge_value &bv, node_handle bp,
+        edge_value &cv, node_handle &cp)
+{
+#ifdef TRACE
+    out.indentation(0);
+    ++top_count;
+    out << ATYPE::name() << " #" << top_count << " begin\n";
+#endif
+
+    _compute(L, in, av, ap, bv, bp, cv, cp);
+
+#ifdef TRACE
+    out << ATYPE::name() << " #" << top_count << " end\n";
+#endif
+
+}
+
+
+template <class EOP, class ATYPE>
+void MEDDLY::arith_pushdn<EOP, ATYPE>::_compute(int L, unsigned in,
+        edge_value av, node_handle A, edge_value bv, node_handle B,
+        edge_value &cv, node_handle &C)
+{
+    // **************************************************************
+    //
+    // Check terminal cases
+    //
+    // **************************************************************
+
+    if ( arg1F->isTerminalNode(A) && arg2F->isTerminalNode(B) )
+    {
+        // A and B are both terminal nodes.
+        // We can stop the recursion if L==0 or
+        // we're not forced to proceed by levels.
+        if ((0==L) || (!forced_by_levels))
+        {
+            ATYPE::apply(av, A, bv, B, cv, C);
+            C = resF->makeRedundantsTo(C, 0, L);
+            return;
+        }
+    }
+
+    if ( ATYPE::stopOnEqualArgs() && (arg1F == arg2F) && (A == B) && (av == bv))
+    {
+        //
+        // Result is <av, A>
+        //
+        ATYPE::makeEqualResult(av, A);
+        MEDDLY_DCASSERT(copy_arg1res);
+        copy_arg1res->compute(L, in, av, A, cv, C);
+        return;
+    }
+
+    if ( ATYPE::simplifiesToFirstArg(av, A, bv, B) )
+    {
+        //
+        // Result is <av, A>
+        //
+        MEDDLY_DCASSERT(copy_arg1res);
+        copy_arg1res->compute(L, in, av, A, cv, C);
+        return;
+    }
+
+    if ( ATYPE::simplifiesToSecondArg(av, A, bv, B) )
+    {
+        //
+        // Result is <bv, B>
+        //
+        MEDDLY_DCASSERT(copy_arg2res);
+        copy_arg2res->compute(L, in, bv, B, cv, C);
+        return;
+    }
+
+    //
+    // Reorder A and B if the operation commutes
+    // and A,B are from the same forest
+    //
+    if ( ATYPE::commutes() && arg1F == arg2F )
+    {
+        if (A > B) {
+            SWAP(A, B);
+            SWAP(av, bv);
+        }
+    }
+
+    // **************************************************************
+    //
+    // Determine level information
+    //
+    // **************************************************************
+    const int Alevel = arg1F->getNodeLevel(A);
+    const int Blevel = arg2F->getNodeLevel(B);
+    const int Clevel = topLevelOf(L, Alevel, Blevel);
+    const int Cnextlevel = resF->isForRelations()
+        ? MXD_levels::downLevel(Clevel)
+        : MDD_levels::downLevel(Clevel)
+    ;
+
+#ifdef TRACE
+    out << ATYPE::name() << " arith_pushdn::compute(" << L << ", " << in << ", "
+        << "<";
+    av.show(out);
+    out << ", " << A << ">, <";
+    bv.show(out);
+    out << ", " << B << ">)\n";
+    out << A << " level " << Alevel << "\n";
+    out << B << " level " << Blevel << "\n";
+    out << "result level " << Clevel << "\n";
+#endif
+
+    // **************************************************************
+    //
+    // Check the compute table
+    //
+    // **************************************************************
+    ct_vector key(forced_by_levels ? 3 : 2);
+    ct_vector res(1);
+    unsigned keyind=0;
+    if (forced_by_levels) {
+        key[keyind++].setI(L);
+    }
+    key[keyind++].set(av);
+    key[keyind++].setN(A);
+    key[keyind++].set(bv);
+    key[keyind]  .setN(B);
+
+    if (ct->findCT(key, res)) {
+        //
+        // compute table hit
+        //
+        res[0].get(cv);
+        C = resF->linkNode(res[1].getN());
+#ifdef TRACE
+        out << "CT hit ";
+        key.show(out);
+        out << " -> ";
+        res.show(out);
+        out << "\n";
+#endif
+        if (L == resF->getNodeLevel(C)) {
+            // Make sure we don't point to a singleton
+            // from the same index.
+            C = resF->redirectSingleton(in, C);
+        } else {
+            C = resF->makeRedundantsTo(C, Clevel, L);
+        }
+        return;
+        //
+        // done compute table hit
+        //
+    }
+
+    // **************************************************************
+    //
+    // Compute table 'miss'; do computation
+    //
+    // **************************************************************
+
+    //
+    // Set up unpacked nodes
+    //
+
+    unpacked_node* Au;
+    if (Alevel != Clevel) {
+        if (arg1F->isIdentityReduced() && Clevel<0) {
+            MEDDLY_DCASSERT(Clevel == L);
+            Au = unpacked_node::newIdentity(arg1F, Clevel, in, A, FULL_ONLY);
+            MEDDLY_DCASSERT(Au->wasIdentity());
+        } else {
+            Au = unpacked_node::newRedundant(arg1F, Clevel, A, FULL_ONLY);
+            MEDDLY_DCASSERT(!Au->wasIdentity());
+        }
+    } else {
+        Au = arg1F->newUnpacked(A, FULL_ONLY);
+        MEDDLY_DCASSERT(!Au->wasIdentity());
+    }
+
+    unpacked_node* Bu;
+    if (Blevel != Clevel) {
+        if (arg2F->isIdentityReduced() && Clevel<0) {
+            MEDDLY_DCASSERT(Clevel == L);
+            Bu = unpacked_node::newIdentity(arg2F, Clevel, in, B, FULL_ONLY);
+            MEDDLY_DCASSERT(Bu->wasIdentity());
+        } else {
+            Bu = unpacked_node::newRedundant(arg2F, Clevel, B, FULL_ONLY);
+            MEDDLY_DCASSERT(!Bu->wasIdentity());
+        }
+    } else {
+        Bu = arg2F->newUnpacked(B, FULL_ONLY);
+        MEDDLY_DCASSERT(!Bu->wasIdentity());
+    }
+
+    MEDDLY_DCASSERT(Au->getSize() == Bu->getSize());
+    unpacked_node* Cu = unpacked_node::newFull(resF, Clevel, Au->getSize());
+
+#ifdef TRACE
+    out << "A: ";
+    Au->show(out, true);
+    out << "\nB: ";
+    Bu->show(out, true);
+    out.indent_more();
+    out.put('\n');
+#endif
+
+    //
+    // Recurse
+    //
+    for (unsigned i=0; i<Cu->getSize(); i++) {
+        node_handle cd;
+        edge_value x;
+        _compute(Cnextlevel, i,
+                EOP::applyOp(av, Au->edgeval(i)), Au->down(i),
+                EOP::applyOp(bv, Bu->edgeval(i)), Bu->down(i),
+                x, cd);
+        Cu->setFull(i, x, cd);
+    }
+
+#ifdef TRACE
+    out.indent_less();
+    out.put('\n');
+    out << ATYPE::name() << " arith_pushdn::compute(" << L << ", " << in << ", "
+        << "<";
+    av.show(out);
+    out << ", " << A << ">, <";
+    bv.show(out);
+    out << ", " << B << ">) done\n";
+    out << "  A: ";
+    Au->show(out, true);
+    out << "\n  B: ";
+    Bu->show(out, true);
+    out << "\n  C: ";
+    Cu->show(out, true);
+    out << "\n";
+#endif
+
+    //
+    // Reduce
+    //
+    resF->createReducedNode(Cu, cv, C);
+#ifdef TRACE
+    out << "reduced to edge <";
+    cv.show(out);
+    out << ", " << C << ">, node is ";
+    resF->showNode(out, C, SHOW_DETAILS);
+    out << "\n";
+#endif
+
+    //
+    // Save result in CT, if we can
+    //
+    if (Au->wasIdentity() || Bu->wasIdentity()) {
+        ct->noaddCT(key);
+    } else {
+        res[0].set(cv);
+        res[1].setN(C);
+        ct->addCT(key, res);
+    }
+
+    //
+    // Cleanup
+    //
+    unpacked_node::Recycle(Bu);
+    unpacked_node::Recycle(Au);
+
+    //
+    // Adjust result
+    //
+    if (L == resF->getNodeLevel(C)) {
+        //
+        // We don't need to add identities or redundants;
+        // just check if we need to avoid a singleton.
+        //
+        C = resF->redirectSingleton(in, C);
+    } else {
+        //
+        // Add nodes but only from Clevel;
+        // if the actual level of C is below Clevel it means
+        // nodes were eliminated in the result forest.
+        //
+        C = resF->makeRedundantsTo(C, Clevel, L);
+    }
+}
+
+
+
+//   **************************************************************
+//  ****************************************************************
 // ******************************************************************
 // ***                                                            ***
 // ***                                                            ***
@@ -1050,8 +1437,8 @@ void MEDDLY::arith_factor<EOP, ATYPE>::_compute(int L, unsigned in,
 // ***                                                            ***
 // ***                                                            ***
 // ******************************************************************
-// ******************************************************************
-// ******************************************************************
+//  ****************************************************************
+//   **************************************************************
 
 // ******************************************************************
 // *                                                                *
@@ -1148,6 +1535,30 @@ namespace MEDDLY {
 
 
 
+//   **************************************************************
+//  ****************************************************************
+// ******************************************************************
+// ***                                                            ***
+// ***                                                            ***
+// ***            Structs for  "factorable" operations            ***
+// ***                                                            ***
+// ***                                                            ***
+// ******************************************************************
+//  ****************************************************************
+//   **************************************************************
+
+
+//   **************************************************************
+//  ****************************************************************
+// ******************************************************************
+// ***                                                            ***
+// ***                                                            ***
+// ***             Structs for "push down" operations             ***
+// ***                                                            ***
+// ***                                                            ***
+// ******************************************************************
+//  ****************************************************************
+//   **************************************************************
 
 // ******************************************************************
 // *                                                                *
