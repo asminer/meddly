d0e1304e8abef84c617d436279a599f53c52187c
Andrew Miner <asminer@iastate.edu>
2025-01-23 23:03:39 -0600

    Updated version number

--- src/defines.h
+++ src/defines.h
@@ -28,6 +28,7 @@
 // #define ALLOW_DEPRECATED_0_17_5  "04/05/2024"
 #define ALLOW_DEPRECATED_0_17_6  "11/11/2024"
 // #define ALLOW_DEPRECATED_0_17_7  "01/23/2025"
+#define ALLOW_DEPRECATED_0_17_8
 
 // ==================================================================
 // ^ This line is important for the release script, don't remove it
