40a5fda3a4e4b398d2f45ad7078bb404141f860c
Andrew Miner <asminer@iastate.edu>
2026-06-04 16:11:12 -0500

    More script table stuff for traditional

--- developers/RunTradSS.sh
+++ developers/RunTradSS.sh
@@ -25,6 +25,63 @@ table_header()
     printf -- "-----------++------------+------------++--------+--------++------------+-----------\n"
 }
 
+show_table()
+{
+    echo
+
+    table_header
+
+    for m in 'phils 800' 'kanban 75' 'slot 20' 'tiles 3x3'
+    do
+        base=`first_word $m`
+
+        tfile=/tmp/$base.trad.txt
+        ffile=/tmp/$base.front.txt
+
+        # echo "$m  /tmp/$base.trad.txt"
+
+        if [ "tiles" == "$base" ]
+        then
+            ttime=`awk '/reachable states construction took/{print $5}' $tfile`
+            ftime=`awk '/reachable states construction took/{print $5}' $ffile`
+        else
+            ttime=`awk '/Reachability set construction took/{print $5}' $tfile`
+            ftime=`awk '/Reachability set construction took/{print $5}' $ffile`
+        fi
+
+        if [ "phils" == "$base" ]
+        then
+            tfinal=`awk '/#Nodes:/{print $2}' $tfile`
+            ffinal=`awk '/#Nodes:/{print $2}' $ffile`
+        fi
+        if [ "kanban" == "$base" ]
+        then
+            tfinal=`awk '/Reachability set uses/{print $4}' $tfile`
+            ffinal=`awk '/Reachability set uses/{print $4}' $ffile`
+        fi
+        if [ "slot" == "$base" ]
+        then
+            tfinal=`awk '/Reachability set uses/{print $4}' $tfile`
+            ffinal=`awk '/Reachability set uses/{print $4}' $ffile`
+        fi
+        if [ "tiles" == "$base" ]
+        then
+            tfinal=`awk '/MDD for reachable states/{print $6}' $tfile`
+            ffinal=`awk '/MDD for reachable states/{print $6}' $ffile`
+        fi
+
+        tpeak=`awk '/MDD stats:/{mdd=1} /peak nodes/{if (mdd) print $1}' $tfile`
+        fpeak=`awk '/MDD stats:/{mdd=1} /peak nodes/{if (mdd) print $1}' $ffile`
+
+
+        # echo "    trad $ttime sec $tfinal final nodes $tpeak peak"
+        # echo "    front $ftime sec $ffinal final nodes $fpeak peak"
+
+        table_row "$m" "$ttime" "$ftime" "$tfinal" "$ffinal" "$tpeak" "$fpeak"
+    done
+    echo
+}
+
 path="examples"
 if [ ! -d examples ]
 then
@@ -60,57 +117,4 @@ $path/slot -front 20 | tee /tmp/slot.front.txt
 $path/tiles --pos --trad 3 3 | tee /tmp/tiles.trad.txt
 $path/tiles --pos --front 3 3 | tee /tmp/tiles.front.txt
 
-
-echo
-
-table_header
-
-for m in 'phils 800' 'kanban 75' 'slot 20' 'tiles 3x3'
-do
-    base=`first_word $m`
-
-    tfile=/tmp/$base.trad.txt
-    ffile=/tmp/$base.front.txt
-
-    # echo "$m  /tmp/$base.trad.txt"
-
-    if [ "tiles" == "$base" ]
-    then
-        ttime=`awk '/reachable states construction took/{print $5}' $tfile`
-        ftime=`awk '/reachable states construction took/{print $5}' $ffile`
-    else
-        ttime=`awk '/Reachability set construction took/{print $5}' $tfile`
-        ftime=`awk '/Reachability set construction took/{print $5}' $ffile`
-    fi
-
-    if [ "phils" == "$base" ]
-    then
-        tfinal=`awk '/#Nodes:/{print $2}' $tfile`
-        ffinal=`awk '/#Nodes:/{print $2}' $ffile`
-    fi
-    if [ "kanban" == "$base" ]
-    then
-        tfinal=`awk '/Reachability set uses/{print $4}' $tfile`
-        ffinal=`awk '/Reachability set uses/{print $4}' $ffile`
-    fi
-    if [ "slot" == "$base" ]
-    then
-        tfinal=`awk '/Reachability set uses/{print $4}' $tfile`
-        ffinal=`awk '/Reachability set uses/{print $4}' $ffile`
-    fi
-    if [ "tiles" == "$base" ]
-    then
-        tfinal=`awk '/MDD for reachable states/{print $6}' $tfile`
-        ffinal=`awk '/MDD for reachable states/{print $6}' $ffile`
-    fi
-
-    tpeak=`awk '/MDD stats:/{mdd=1} /peak nodes/{if (mdd) print $1}' $tfile`
-    fpeak=`awk '/MDD stats:/{mdd=1} /peak nodes/{if (mdd) print $1}' $ffile`
-
-
-    # echo "    trad $ttime sec $tfinal final nodes $tpeak peak"
-    # echo "    front $ftime sec $ffinal final nodes $fpeak peak"
-
-    table_row "$m" "$ttime" "$ftime" "$tfinal" "$ffinal" "$tpeak" "$fpeak"
-done
-echo
+show_table | tee .trad_ss.txt
